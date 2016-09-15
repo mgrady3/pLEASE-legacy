f83009fef2fb5d6153864ae630a32dd1bb5dceaa
Maxwell <max.grady@gmail.com>
2016-09-15 12:43:47 -0400

    Change python version checking in main.py to be more pythonic and use sys.version_info instead of doing a hacky string comparison from sys.version

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Attempt to set QT_API version through sip before loading any PyQt modules
 import sys
-if sys.version.startswith('2.7'):
+if sys.version_info[0] == 2 and sys.version_info[1] >= 7:  # python 2.7
     try:
         import sip
         sip.setapi('QString', 2)
@@ -8,7 +8,7 @@ if sys.version.startswith('2.7'):
     except ImportError:
         print("Error: Failed to import sip; this is required by PyQt; Exiting ...")
         sys.exit(1)
-elif sys.version.startswith('3'):
+elif sys.version_info[0] == 3:  # python 3
     try:
         import sip
         sip.setapi('QString', 2)  # redundant in Py3
